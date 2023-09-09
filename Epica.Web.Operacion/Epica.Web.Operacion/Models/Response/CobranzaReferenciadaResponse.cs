38394fecc04f0cc0416898fb334a0fae3b87488f
Administrador <Administrador>
2023-09-08 22:28:04 -0600

    Se agregaron las columnas de Numero de cuenta clabe, alias y numero de tarjeta en cobranzas referenciadas.

--- Epica.Web.Operacion/Epica.Web.Operacion/Models/Response/CobranzaReferenciadaResponse.cs
+++ Epica.Web.Operacion/Epica.Web.Operacion/Models/Response/CobranzaReferenciadaResponse.cs
@@ -8,14 +8,34 @@ namespace Epica.Web.Operacion.Models.Response;
 
 public class CobranzaReferenciadaResponse
 {
+    
     public int id { get; set; }
     public int idCuentaAhorroPadre { get; set; }
-    public string noCuentaPadre { get; set; }
-    public string nombre { get; set; }
-    public string fechaAlta { get; set; }
+    public string? noCuentaPadre { get; set; }
+    public string? nombre { get; set; }
+    public string? fechaAlta { get; set; }
     public int idMedioPago { get; set; }
-    public string numeroReferencia { get; set; }
-    public string DescripcionPago { get; set; }
+    public string? numeroReferencia { get; set; }
+    public string? DescripcionPago { get; set; }
+
+    private string? cuentaClabe;
+    public string CuentaClabe
+    {
+        get => string.IsNullOrEmpty(cuentaClabe) ? "N/A" : cuentaClabe;
+        set => cuentaClabe = value;
+    }
+    private string? noTarjeta;
+    public string NoTarjeta
+    {
+        get => !string.IsNullOrEmpty(noTarjeta) ? new string('*', noTarjeta.Length - 4) + noTarjeta.Substring(noTarjeta.Length - 4) : string.Empty;
+        set => noTarjeta = value;
+    }
+    private string? alias;
+    public string Alias
+    {
+        get => string.IsNullOrEmpty(alias) ? "N/A" : alias;
+        set => alias = value;
+    }
     public string estatus { get; set; }
 }
 
