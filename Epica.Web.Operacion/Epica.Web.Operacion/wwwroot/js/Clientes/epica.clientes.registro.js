68e73910dae209644efe129eeee1051fbdc0e404
SergioOliveraS <drasergatich@gmail.com>
2023-09-19 14:41:31 -0600

    *Se elimina el modal de validación de token al guardar registros de clientes, transacciones y tarjetas

--- Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/Clientes/epica.clientes.registro.js
+++ Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/Clientes/epica.clientes.registro.js
@@ -20,9 +20,9 @@ var Init = function () {
                 weekLabel: "S"
             }
         });
-
         $(document).on('click', '#btnGuardarCliente', function (e) {
             var requiredFields = document.querySelectorAll('[required]');
+
             var allFieldsFilled = true;
 
             requiredFields.forEach(function (field) {
@@ -32,55 +32,11 @@ var Init = function () {
             });
 
             if (allFieldsFilled) {
-                // Realiza la validación del token antes de abrir el modal de confirmación
-                Swal.fire({
-                    title: '<span class="swal-title-custom"><b>Valida tu identidad</b></span>',
-                    text: 'Por favor, ingrese su token y código de seguridad:',
-                    html:
-                        '<label for="swal-input1" class="swal-label"><b>Token:</b></label>' +
-                        '<input id="swal-input1" class="swal2-input" style="font-size:14px;width:250px"  placeholder="Token">' +
-                        '<div style="margin-top: 20px;"></div>' +
-                        '<label for="swal-input2" class="swal-label"><b>Código de Seguridad:</b></label>' +
-                        '<input id="swal-input2" class="swal2-input" style="font-size:14px;width:250px" type="password" placeholder="Código de Seguridad">',
-                    showCancelButton: true,
-                    confirmButtonColor: '#0493a8',
-                    confirmButtonText: 'Aceptar',
-                    cancelButtonText: 'Cancelar',
-                    preConfirm: () => {
-                        const swalInput1 = Swal.getPopup().querySelector('#swal-input1').value;
-                        const swalInput2 = Swal.getPopup().querySelector('#swal-input2').value;
-                        return [swalInput1, swalInput2];
-                    }
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        const [tokenInput, codigoInput] = result.value;
-
-                        // Realiza la validación del token y código de seguridad
-                        $.ajax({
-                            url: '/Autenticacion/ValidarTokenYCodigo',
-                            async: true,
-                            cache: false,
-                            type: 'POST',
-                            data: { token: tokenInput, codigo: codigoInput },
-                            success: function (validationResult) {
-                                if (validationResult.mensaje === true) {
-                                    // La validación fue exitosa, abre el modal de confirmación
-                                    $('#confirmModal').modal('show');
-                                } else {
-                                    toastr.error('Token o código de seguridad incorrectos', "");
-                                }
-                            },
-                            error: function () {
-                                toastr.error('Hubo un error en la validación del token y código de seguridad', "");
-                            }
-                        });
-                    }
-                });
+                $('#confirmModal').modal('show');
             } else {
                 toastr.error('Por favor, complete todos los campos obligatorios', "");
             }
         });
-
         $(document).on('click', '#btnCerrarModal, #btnCerrarModal2, .modal-close', function (e) {
             $('#confirmModal').modal('hide');
         });
--- Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/Tarjetas/epica.tarjetas.registroTarjetaCliente.js
+++ Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/Tarjetas/epica.tarjetas.registroTarjetaCliente.js
@@ -5,6 +5,7 @@ const yearVigencia = selectYearVigencia.value;
 
 $(document).on('click', '#btnGuardarTarjeta', function (e) {
     var requiredFields = document.querySelectorAll('[required]');
+
     var allFieldsFilled = true;
 
     requiredFields.forEach(function (field) {
@@ -14,50 +15,7 @@ $(document).on('click', '#btnGuardarTarjeta', function (e) {
     });
 
     if (allFieldsFilled) {
-        // Realiza la validación del token antes de abrir el modal de confirmación
-        Swal.fire({
-            title: '<span class="swal-title-custom"><b>Valida tu identidad</b></span>',
-            text: 'Por favor, ingrese su token y código de seguridad:',
-            html:
-                '<label for="swal-input1" class="swal-label"><b>Token:</b></label>' +
-                '<input id="swal-input1" class="swal2-input" style="font-size:14px;width:250px"  placeholder="Token">' +
-                '<div style="margin-top: 20px;"></div>' +
-                '<label for="swal-input2" class="swal-label"><b>Código de Seguridad:</b></label>' +
-                '<input id="swal-input2" class="swal2-input" style="font-size:14px;width:250px" type="password" placeholder="Código de Seguridad">',
-            showCancelButton: true,
-            confirmButtonColor: '#0493a8',
-            confirmButtonText: 'Aceptar',
-            cancelButtonText: 'Cancelar',
-            preConfirm: () => {
-                const swalInput1 = Swal.getPopup().querySelector('#swal-input1').value;
-                const swalInput2 = Swal.getPopup().querySelector('#swal-input2').value;
-                return [swalInput1, swalInput2];
-            }
-        }).then((result) => {
-            if (result.isConfirmed) {
-                const [tokenInput, codigoInput] = result.value;
-
-                // Realiza la validación del token y código de seguridad
-                $.ajax({
-                    url: '/Autenticacion/ValidarTokenYCodigo',
-                    async: true,
-                    cache: false,
-                    type: 'POST',
-                    data: { token: tokenInput, codigo: codigoInput },
-                    success: function (validationResult) {
-                        if (validationResult.mensaje === true) {
-                            // La validación fue exitosa, abre el modal de confirmación
-                            $('#confirmModal').modal('show');
-                        } else {
-                            toastr.error('Token o código de seguridad incorrectos', "");
-                        }
-                    },
-                    error: function () {
-                        toastr.error('Hubo un error en la validación del token y código de seguridad', "");
-                    }
-                });
-            }
-        });
+        $('#confirmModal').modal('show');
     } else {
         toastr.error('Por favor, complete todos los campos obligatorios', "");
     }
@@ -82,8 +40,8 @@ $(document).on('click', '#btnBuscarCliente', function () {
 
             results.forEach(function (item) {
                 const option = document.createElement('option');
-                option.value = item.id; 
-                option.text = item.descripcion; 
+                option.value = item.id;
+                option.text = item.descripcion;
                 selectClientes.appendChild(option);
             });
 
