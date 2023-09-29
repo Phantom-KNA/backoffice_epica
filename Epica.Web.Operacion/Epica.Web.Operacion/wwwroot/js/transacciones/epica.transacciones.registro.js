3e6dfce0098e186e06ece957973c832dc37387d6
SergioOliveraS <drasergatich@gmail.com>
2023-09-29 04:34:18 -0600

    *Se añaden validaciones de números y letras en registro de transacciones

--- Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/transacciones/epica.transacciones.registro.js
+++ Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/transacciones/epica.transacciones.registro.js
@@ -1,6 +1,24 @@
 ﻿"use strict";
 toastr.options.preventDuplicates = true;
 
+function validateInput(inputElement) {
+    var inputValue = inputElement.value;
+    var cleanValue = inputValue.replace(/[^A-Za-z]/g, '');
+
+    if (cleanValue !== inputValue) {
+        inputElement.value = cleanValue;
+    }
+}
+
+function validateNumbersInput(inputElement) {
+    var inputValue = inputElement.value;
+    var cleanValue = inputValue.replace(/[^0-9]/g, '');
+
+    if (cleanValue !== inputValue) {
+        inputElement.value = cleanValue;
+    }
+}
+
 var Init = function () {
     var init = function () {
 
@@ -25,17 +43,44 @@ var Init = function () {
             var requiredFields = document.querySelectorAll('[required]');
 
             var allFieldsFilled = true;
+            var validationFailed = false;
+            var firstErrorMessage = null;
 
             requiredFields.forEach(function (field) {
                 if (!field.value) {
                     allFieldsFilled = false;
+                    var errorMessage = field.getAttribute("data-error-message") || "Este campo";
+                    if (!firstErrorMessage) {
+                        firstErrorMessage = errorMessage + ' es obligatorio';
+                    }
+                }
+
+                if (field.id === "NombreOrdenante" || field.id === "Concepto") {
+                    if (field.value.length < 3 || field.value.length > 20) {
+                        var errorMessage = field.getAttribute("data-error-message") || "Este campo";
+                        if (!firstErrorMessage) {
+                            firstErrorMessage = errorMessage + ' Ingresa un nombre válido';
+                        }
+                        validationFailed = true;
+                    }
+                }
+
+                if (field.id === "NoCuentaBeneficiario") {
+                    if (field.value.length < 16 || field.value.length > 18) {
+                        var errorMessage = field.getAttribute("data-error-message") || "Este campo";
+                        if (!firstErrorMessage) {
+                            firstErrorMessage = errorMessage + ' Ingresa un número de cuenta válido';
+                        }
+                        validationFailed = true;
+                    }
                 }
             });
 
-            if (allFieldsFilled) {
-                $('#confirmModal').modal('show');
+
+            if (!allFieldsFilled || validationFailed) {
+                toastr.error(firstErrorMessage, "");
             } else {
-                toastr.error('Por favor, complete todos los campos obligatorios', "");
+                $('#confirmModal').modal('show');
             }
         });
         $(document).on('click', '#btnCerrarModal, #btnCerrarModal2, .modal-close', function (e) {
