ff532828f4932733ed3aadeab82cea0f231e3c11
SergioOliveraS <drasergatich@gmail.com>
2023-09-29 03:57:11 -0600

    *Se añaden validaciones en vista de registro de personas físicas

--- Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/Clientes/epica.clientes.registro.js
+++ Epica.Web.Operacion/Epica.Web.Operacion/wwwroot/js/Clientes/epica.clientes.registro.js
@@ -1,6 +1,23 @@
 ﻿"use strict";
 toastr.options.preventDuplicates = true;
 
+function validateInput(inputElement) {
+    var inputValue = inputElement.value;
+    var cleanValue = inputValue.replace(/[^A-Za-z]/g, '');
+
+    if (cleanValue !== inputValue) {
+        inputElement.value = cleanValue;
+    }
+}
+
+function validateNumbersInput(inputElement) {
+    var inputValue = inputElement.value;
+    var cleanValue = inputValue.replace(/[^0-9]/g, '');
+
+    if (cleanValue !== inputValue) {
+        inputElement.value = cleanValue;
+    }
+}
 
 var Init = function () {
     var init = function () {
@@ -22,27 +39,80 @@ var Init = function () {
                 weekLabel: "S"
             }
         });
+
         $(document).on('click', '#btnGuardarCliente', function (e) {
             var requiredFields = document.querySelectorAll('[required]');
 
             var allFieldsFilled = true;
+            var validationFailed = false;
+            var firstErrorMessage = null;
 
             requiredFields.forEach(function (field) {
                 if (!field.value) {
                     allFieldsFilled = false;
+                    var errorMessage = field.getAttribute("data-error-message") || "Este campo";
+                    if (!firstErrorMessage) {
+                        firstErrorMessage = errorMessage + ' es obligatorio';
+                    }
+                }
+
+                if (field.id === "Nombre" || field.id === "ApellidoPaterno" || field.id === "ApellidoMaterno" || field.id === "PaisNacimiento" || field.id === "Colonia" || field.id === "DelegacionMunicipio" || field.id === "CiudadEstado" || field.id === "Calle" || field.id === "PrimeraCalle" || field.id === "SegundaCalle" || field.id === "Puesto" || field.id === "Observaciones") {
+                    if (field.value.length < 3 || field.value.length > 20) {
+                        var errorMessage = field.getAttribute("data-error-message") || "Este campo";
+                        if (!firstErrorMessage) {
+                            firstErrorMessage = errorMessage + ' debe tener entre 3 y 20 caracteres';
+                        }
+                        validationFailed = true;
+                    }
+                }
+
+                if (field.id === "Email") {
+                    var emailValue = field.value;
+                    var emailPattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/;
+                    if (!emailPattern.test(emailValue)) {
+                        var errorMessage = field.getAttribute("data-error-message") || "Email";
+                        if (!firstErrorMessage) {
+                            firstErrorMessage = errorMessage + ' no es válido';
+                        }
+                        validationFailed = true;
+                    }
+                }
+
+                if (field.id === "Telefono") {
+                    var phoneNumber = field.value;
+                    if (!/^\d{10}$/.test(phoneNumber)) {
+                        var errorMessage = field.getAttribute("data-error-message") || "Teléfono Móvil";
+                        if (!firstErrorMessage) {
+                            firstErrorMessage = errorMessage + ' debe tener 10 dígitos';
+                        }
+                        validationFailed = true;
+                    }
+                }
+                if (field.id === "IngresoMensual" || field.id === "MontoMaximo") {
+                    var numericValue = parseFloat(field.value);
+                    if (isNaN(numericValue) || numericValue < 100 || numericValue > 9999999) {
+                        var errorMessage = field.getAttribute("data-error-message") || "Valor";
+                        if (!firstErrorMessage) {
+                            firstErrorMessage = errorMessage + ' debe estar entre 100 y 9999999';
+                        }
+                        validationFailed = true;
+                    }
                 }
             });
 
-            if (allFieldsFilled) {
-                $('#confirmModal').modal('show');
+
+            if (!allFieldsFilled || validationFailed) {
+                toastr.error(firstErrorMessage, "");
             } else {
-                toastr.error('Por favor, complete todos los campos obligatorios', "");
+                $('#confirmModal').modal('show');
             }
         });
+
         $(document).on('click', '#btnCerrarModal, #btnCerrarModal2, .modal-close', function (e) {
             $('#confirmModal').modal('hide');
         });
     }
+
     return {
         init: function () {
             init();
